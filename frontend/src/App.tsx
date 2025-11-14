2e543f1080b4a5546850f0f1065c54fe51333f87
Harshad <harshadbondarde2206@gmail.com>
2025-11-14 21:47:22 +0530

    add excel file function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,41 @@
 import { useState } from 'react'
 import { FaArrowUpFromBracket } from "react-icons/fa6";
+import * as XLSX from "xlsx";
 function App() {
+  const [file,setFile]=useState<File|null>(null);
+
+  // const handleFileChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+  //   const files=e.target.files?.[0];
+  //   if(!files)  return;
+  //   setFile(files);
+  //   console.log(files);
+    
+  //   const fileReader=new FileReader();
+  //   fileReader.onload=(event)=>{
+  //     const fileContent=event.target?.result;
+  //     console.log(fileContent);
+  //   };
+  // }
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    console.log(file)
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const arrayBuffer = event.target?.result;
+      console.log(arrayBuffer)
+      if (!arrayBuffer) return;
+
+      const workbook = XLSX.read(arrayBuffer, { type: "array" });
+      const sheet = workbook.Sheets[workbook.SheetNames[0]];
+
+      const jsonData = XLSX.utils.sheet_to_json(sheet);
+      console.log("Excel JSON data:", jsonData);
+    };
+
+    reader.readAsArrayBuffer(file); 
+  };
 
   return (
     <div className=''>
@@ -16,7 +51,7 @@ function App() {
               Add Candidates to Database
             </div>
             <div>
-              <input type="file" id="file" accept='file/*' className='hidden'/>
+              <input type="file" id="file" accept='file/*' className='hidden' onChange={handleFileChange}/>
               <label 
                 htmlFor='file'
                 className='h-54 w-full border-2 border-gray-400 p-7 flex justify-center rounded shadow-md bg-gray-100 mt-5 cursor-pointer'>
