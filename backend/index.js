c38fef034663cc28d9695af10142868a5bd04e6a
Harshad <harshadbondarde2206@gmail.com>
2025-11-15 18:37:35 +0530

    ready for production

--- backend/index.js
+++ backend/index.js
@@ -22,12 +22,12 @@ app.use(express.json({ limit:'50mb' }))
 app.use("/api/v1",mainRoutes)
 
 
-// if(process.env.MODE=='production'){
-//     app.use(express.static(path.join(dir,"/frontend/dist")))
-// }
-// app.get("*",(req,res)=>{
-//     res.sendFile(path.join(dir,"frontend","dist","index.html"))
-// })
+if(process.env.MODE=='production'){
+    app.use(express.static(path.join(dir,"/frontend/dist")))
+    app.get("/.*/",(req,res)=>{
+        res.sendFile(path.join(dir,"frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     connectDB()
